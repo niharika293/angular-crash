80b025e8802ab150f96cf4973c464e798bad6107
Niharika Gurnani <niharika.gurnani@solvezy.com>
2025-02-20 17:56:17 +0530

    Add about, footer components & their routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskComponent } from './components/task/task.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : TaskComponent},
+  {path : 'about', component : AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
